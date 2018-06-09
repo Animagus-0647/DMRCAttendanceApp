94f93b6c41f2029749b9ea7865c4c51c95adaa16
Kumar Archit <kumararchit16@gmail.com>
2018-06-10 00:05:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nishantikagarg/dmrcattendance/LeaveStatus.java
@@ -0,0 +1,13 @@
+package com.example.nishantikagarg.dmrcattendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeaveStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leave_status);
+    }
+}
